1f9a8c8e1a5a1001c8c79ebdb8f7be0dafaf4dd4
Federico Jose Diaz Guanoluiza <fdiaz-gu@c3r3s4.42madrid.com>
2024-04-16 16:17:14 +0200

    end .c mutex dead_lock

--- src/end.c
+++ src/end.c
@@ -6,7 +6,7 @@
 /*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 15:14:52 by fdiaz-gu          #+#    #+#             */
-/*   Updated: 2024/04/16 15:15:07 by fdiaz-gu         ###   ########.fr       */
+/*   Updated: 2024/04/16 16:16:56 by fdiaz-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ void	destroy_mutex(t_philo_program *prog)
 	i = 0;
 	pthread_mutex_destroy(&prog->meal_lock);
 	pthread_mutex_destroy(&prog->print_lock);
+	pthread_mutex_destroy(&prog->dead_lock);
 	while (i < prog->nb_philos)
 	{
 		pthread_mutex_destroy(&prog->forks[i]);
